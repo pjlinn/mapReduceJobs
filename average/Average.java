4006b99a231b3c7291bd28dd6834b87d1b517624
pjlinn <p.linnehan@gmail.com>
2014-01-07 21:24:53 -0500

    MR avg update

--- average/Average.java
+++ average/Average.java
@@ -1,11 +1,25 @@
 /*
 	Finds the average of a set of numbers
+
+	-----------------
+
+	To compile:
+		javac -cp ~/Documents/dataMiningPrograms\
+		/hadoop-1.2.1/hadoop-core-1.2.1.jar -d average/ Average.java
+
+	To jar:
+		jar -cvf average.jar -C average/ .
+
+	To Run:
+		bin/hadoop jar ~/Documents/gitHub/mapReduceJobs/average/average.jar \
+		Average /input/nums.txt /output/
 */
 
 
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -20,10 +34,11 @@ import java.util.StringTokenizer;
 public class Average {
 	// Class Mapper<KEYIN,VALUEIN,KEYOUT,VALUEOUT>
 	static public class Map 
-	extends Mapper<LongWritable,Text,Text,IntWritable> {
+	extends Mapper<LongWritable,Text,DoubleWritable,DoubleWritable> {
 
-	private final static Text singleKey = new Text("Average");
-	private IntWritable num = new IntWritable();
+	// private final static Text singleKey = new Text("Average");
+	private final DoubleWritable one = new DoubleWritable(1);
+	private DoubleWritable num = new DoubleWritable();
 
 		@Override
 		public void map(LongWritable key, Text value, Context context)	
@@ -33,31 +48,37 @@ public class Average {
 
 			StringTokenizer tokenizer = new StringTokenizer(line);
 
-			num.set(Integer.parseInt(tokenizer.nextToken()));
+			num.set(Double.parseDouble(tokenizer.nextToken()));
 
-			context.write(singleKey, num);
+			context.write(one, num);
 
 		}
 
 	}
 
 	static public class Reduce 
-	extends Reducer<Text,IntWritable,Text,IntWritable> {
+	extends Reducer<DoubleWritable,DoubleWritable,DoubleWritable,DoubleWritable> {
 
 		@Override
-		public void reduce(Text key, Iterable<IntWritable> values, Context context)
+		public void reduce(DoubleWritable key, Iterable<DoubleWritable> values, Context context)
 		throws IOException, InterruptedException {
 
-			int sum = 0;
-			int counter = 0;
+			double sum = 0;
+			double counter = 0;
+			double avg;
 
-			for (IntWritable value : values) {
+			// Text avg = new Text("Average");
+			DoubleWritable iSum = new DoubleWritable();
+			DoubleWritable dKey = new DoubleWritable(key.get());
+
+			for (DoubleWritable value : values) {
 				sum += value.get();
-				counter++;
+				counter += dKey.get();
 			}
 
-			sum = sum / counter; // Rounds off decimal - fix
-			context.write(key, new IntWritable(sum));
+			avg = sum / counter; // Rounds off decimal - fix
+			iSum.set(counter);
+			context.write(iSum, new DoubleWritable(avg));
 		}
 	}
 
@@ -71,10 +92,11 @@ public class Average {
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
 		job.setMapperClass(Map.class);
+		// job.setCombinerClass(Reduce.class); Doesn't work
 		job.setReducerClass(Reduce.class);
 
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+		job.setOutputKeyClass(DoubleWritable.class);
+		job.setOutputValueClass(DoubleWritable.class);
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
